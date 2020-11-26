dfc4291c76cfca5864639feda87a5683b8a2404c
outerspace <hello@outerspace.pl>
2020-11-26 16:29:30 +0100

    project init

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.position = new Vector3(transform.position.x + .1f, transform.position.y, transform.position.z);
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.position = new Vector3(transform.position.x - .1f, transform.position.y, transform.position.z);
+        }
+    }
+}
